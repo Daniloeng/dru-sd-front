59021d1433fe89fddd38810c643632266d495a49
GILSON da Silva <gilsonsilva.cintra@gmail.com>
2019-06-03 21:03:58 -0300

    consulta dru com dados do banco de dados

--- src/pages/consulta/consulta.ts
+++ src/pages/consulta/consulta.ts
@@ -1,92 +1,35 @@
+import { ConsultaServiceProvider } from '../../providers/consulta-service/consulta-service';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { TabsPage } from './../tabs/tabs';
-import { ContactPage } from './../contact/contact';
 import { RequestOptions } from '@angular/http';
 import { LoginServiceProvider } from './../../providers/login-service/login-service';
 import { HomePage } from './../home/home';
 import { FormBuilder } from '@angular/forms';
 import { CookieService } from 'angular2-cookie/core';
-import { DruServiceProvider } from './../../providers/dru-service/dru-service';
+import { ContactPage } from './../contact/contact';
 
-
-
-/**
- * Generated class for the ConsultaPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
-  selector: 'page-consulta',
+  selector: 'consulta-page',
   templateUrl: 'consulta.html',
+  providers: [ConsultaServiceProvider]
 })
 export class ConsultaPage {
+  public registros: any;
+  constructor(public navCtrl: NavController, public consultaService: ConsultaServiceProvider) {
 
-  public hasInfo: boolean = false;
-  public drus: any;
-  public cpf: any;
-  public loading: any;
-
-
-  constructor(
-    public navCtrl: NavController,
-    public navParams: NavParams,
-    public formBuilder: FormBuilder,
-    public nav: NavController,
-    private loginService: LoginServiceProvider,
-    private cookieService: CookieService,
-    private requestOptions: RequestOptions,
-    public druService: DruServiceProvider,
-    public loadingController: LoadingController
-  ) {
-    this.loading = this.loadingController.create();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ConsultaPage');
-    this.listarDRU();
+  ionViewWillEnter() {
+    this.registros = [];
+    this.consultaService.getRegistros().subscribe(
+      response => this.registros = response
+    );
   }
 
   doClickContact() {
     this.navCtrl.setRoot(ContactPage);
-  }
-
-  getInfo() {
-    this.drus = []
-    
-    this.loading.present();
-    this.druService.getDRU(this.cpf).subscribe(
-      response => { this.drus = response;
-         this.hasInfo = true;
-         this.loading.dismiss();
-        },
-      error => {console.warn(error  )},
-      () => { this.loading.dismiss(); }
-    );
-    console.log(this.drus);
-  }
-
-  listarDRU() {
-
-
-    this.drus = []
-
-    this.loading.present();
-
-    this.druService.ListarDRUS().subscribe(
-      response => {
-        this.drus = response; this.hasInfo = true; 
-        console.log(JSON.stringify(this.drus));
-        this.loading.dismiss();},
-      error => {console.warn(error);
-        this.loading.dismiss();
-      }
-    );
-    
-   }
+  }  
 
 }
---
+++ src/providers/consulta-service/consulta-service.ts
@@ -0,0 +1,24 @@
+import { Observable } from 'rxjs/Observable';
+import { Utils } from './../../entity/Utils';
+import { Injectable } from '@angular/core';
+import { Headers, Http, RequestOptions } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ConsultaServiceProvider {
+
+    private consultaUrl: string;
+    public handleError: any;
+
+    constructor(public http: HttpClient) {
+        this.consultaUrl = Utils.getUrlBackend() + "dru/";
+    }
+
+    public getRegistros() {
+        console.log((this.consultaUrl));
+        return this.http.get(this.consultaUrl);
+    }
+
+
+}
